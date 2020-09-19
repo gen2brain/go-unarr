07adaf4ed7346744aff1d0a37a94d1251afb2676
AJ ONeal <coolaj86@gmail.com>
2020-09-19 13:24:24 -0600

    add version info for goreleaser

--- cmd/unarr/unarr.go
+++ cmd/unarr/unarr.go
@@ -7,10 +7,21 @@ import (
 	"github.com/gen2brain/go-unarr"
 )
 
+// version info as per https://goreleaser.com/customization/build/
+var (
+	version = "v0.0.0-LOCAL"
+	commit  = "g0000000"
+	date    = "0000-00-00T00:00:00Z"
+)
+
 func main() {
+	if 2 == len(os.Args) && "version" == os.Args[1] {
+		fmt.Printf("Foobar %s %s (%s)\n", version, commit, date)
+	}
+
 	if 3 != len(os.Args) {
-		fmt.Fprintf(os.Stderr, "Usage: unarr <archive>    <local directory>\n")
-		fmt.Fprintf(os.Stderr, "Usage: unarr ./example.7z /tmp/example/\n")
+		fmt.Fprintf(os.Stderr, "Usage:   unarr <archive>    <local directory>\n")
+		fmt.Fprintf(os.Stderr, "Example: unarr ./example.7z /tmp/example/\n")
 		os.Exit(1)
 		return
 	}
