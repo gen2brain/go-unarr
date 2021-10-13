6b172e56ebee93916f1093f781e7434d50310a44
Milan Nikolic <gen2brain@gmail.com>
2021-10-14 01:12:56 +0200

    Remove lto

--- unarrc/unarrc_cgo.go
+++ unarrc/unarrc_cgo.go
@@ -3,6 +3,6 @@ package unarrc
 /*
 #cgo !debug CFLAGS: -DNDEBUG
 #cgo CFLAGS: -DHAVE_7Z -DHAVE_ZLIB -DHAVE_BZIP2 -D_7ZIP_PPMD_SUPPPORT
-#cgo CFLAGS: -Iexternal/lzma -Iexternal/zlib -Iexternal/bzip2 -Iexternal/unarr -fomit-frame-pointer -flto
+#cgo CFLAGS: -Iexternal/lzma -Iexternal/zlib -Iexternal/bzip2 -Iexternal/unarr -fomit-frame-pointer
 */
 import "C"
