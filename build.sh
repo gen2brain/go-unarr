e9175db65e0de03307a401e7d120686b0752ac0c
Milan Nikolic <gen2brain@gmail.com>
2021-10-14 02:02:44 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This crooss-compiled for all supported cross-compile targets with goreleaser
+# This cross-compiled for all supported cross-compile targets with goreleaser
 # from MacOS, like so:
 #     goreleaser --snapshot --skip-publish --rm-dist
 
@@ -10,13 +10,13 @@ mkdir -p dist
 
 export CGO_ENABLED=1
 printf "Build for Darwin (macOS) ...            "
-GOARCH=amd64 GOOS=darwin                              go build -o dist/unarr_darwin_amd64 -ldflags '-w' cmd/unarr/unarr.go
+GOARCH=amd64 GOOS=darwin go build -o dist/unarr_darwin_amd64 -ldflags '-w' cmd/unarr/unarr.go
 echo "dist/unarr_darwin_amd64"
 
 # Install x86_64-w64-mingw32-gcc:
 #     brew install mingw-w64
 printf "Build for Windows ...                   "
-GOARCH=amd64 GOOS=windows CC=x86_64-w64-mingw32-gcc   go build -o dist/unarr.exe -ldflags '-w' cmd/unarr/unarr.go
+GOARCH=amd64 GOOS=windows CC=x86_64-w64-mingw32-gcc go build -o dist/unarr.exe -ldflags '-w' cmd/unarr/unarr.go
 echo "dist/unarr.exe"
 
 # Install x86_64-linux-musl-gcc:
