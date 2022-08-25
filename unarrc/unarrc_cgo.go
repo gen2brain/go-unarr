db3833a5faf2a3539c3ee4a0300e0ea38109daf3
Milan Nikolic <gen2brain@gmail.com>
2022-08-25 14:40:06 +0200

    Set file offset bits, fixes #32

--- unarrc/unarrc_cgo.go
+++ unarrc/unarrc_cgo.go
@@ -4,5 +4,6 @@ package unarrc
 #cgo !debug CFLAGS: -DNDEBUG
 #cgo CFLAGS: -DHAVE_7Z -DHAVE_ZLIB -DHAVE_BZIP2 -D_7ZIP_PPMD_SUPPPORT
 #cgo CFLAGS: -Iexternal/lzma -Iexternal/zlib -Iexternal/bzip2 -Iexternal/unarr -fomit-frame-pointer
+#cgo amd64 arm64 arm64be loong64 mips64 mips64le ppc64 ppc64le riscv64 s390x sparc64 CFLAGS: -D_FILE_OFFSET_BITS=64
 */
 import "C"
