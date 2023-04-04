4d5d46ad7d402fd51f4aff29381944a18e9ef6b4
Milan Nikolic <gen2brain@gmail.com>
2023-04-04 11:18:54 +0200

    Use c99 instead of gnu99, issue #38

--- unarrc/unarrc_cgo.go
+++ unarrc/unarrc_cgo.go
@@ -2,7 +2,7 @@ package unarrc
 
 /*
 #cgo !debug CFLAGS: -DNDEBUG
-#cgo CFLAGS: -std=gnu99 -DHAVE_7Z -DHAVE_ZLIB -DHAVE_BZIP2 -D_7ZIP_PPMD_SUPPPORT
+#cgo CFLAGS: -std=c99 -DHAVE_7Z -DHAVE_ZLIB -DHAVE_BZIP2 -D_7ZIP_PPMD_SUPPPORT
 #cgo CFLAGS: -Iexternal/lzma -Iexternal/zlib -Iexternal/bzip2 -Iexternal/unarr -fomit-frame-pointer
 #cgo amd64 arm64 arm64be loong64 mips64 mips64le ppc64 ppc64le riscv64 s390x sparc64 CFLAGS: -D_FILE_OFFSET_BITS=64
 */
