3daefafa253f8b63e1f5096a06b575d63d19165e
Milan Nikolic <gen2brain@gmail.com>
2023-10-16 05:01:30 +0200

    Fix warnings, issue #38

--- unarrc/unarrc_cgo.go
+++ unarrc/unarrc_cgo.go
@@ -2,8 +2,13 @@ package unarrc
 
 /*
 #cgo !debug CFLAGS: -DNDEBUG
-#cgo CFLAGS: -std=c99 -DHAVE_7Z -DHAVE_ZLIB -DHAVE_BZIP2 -D_7ZIP_PPMD_SUPPPORT -Wno-typedef-redefinition -Wno-deprecated-non-prototype
+#cgo CFLAGS: -std=c99 -DHAVE_7Z -DHAVE_ZLIB -DHAVE_BZIP2 -D_7ZIP_PPMD_SUPPPORT
 #cgo CFLAGS: -Iexternal/lzma -Iexternal/zlib -Iexternal/bzip2 -Iexternal/unarr -fomit-frame-pointer
 #cgo amd64 arm64 arm64be loong64 mips64 mips64le ppc64 ppc64le riscv64 s390x sparc64 CFLAGS: -D_FILE_OFFSET_BITS=64
+
+#if defined(__clang__) && __clang_major__ >= 16
+#cgo CFLAGS: -Wno-typedef-redefinition -Wno-deprecated-non-prototype
+#endif
+
 */
 import "C"
