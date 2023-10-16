551f48c2a71bd1ec322e12091b84ba115881faeb
Milan Nikolic <gen2brain@gmail.com>
2023-10-16 05:43:22 +0200

    Update zlib

--- unarrc/unarrc_cgo.go
+++ unarrc/unarrc_cgo.go
@@ -3,12 +3,7 @@ package unarrc
 /*
 #cgo !debug CFLAGS: -DNDEBUG
 #cgo CFLAGS: -std=c99 -DHAVE_7Z -DHAVE_ZLIB -DHAVE_BZIP2 -D_7ZIP_PPMD_SUPPPORT
-#cgo CFLAGS: -Iexternal/lzma -Iexternal/zlib -Iexternal/bzip2 -Iexternal/unarr -fomit-frame-pointer
+#cgo CFLAGS: -Iexternal/lzma -Iexternal/zlib -Iexternal/bzip2 -Iexternal/unarr -fomit-frame-pointer -Wno-typedef-redefinition
 #cgo amd64 arm64 arm64be loong64 mips64 mips64le ppc64 ppc64le riscv64 s390x sparc64 CFLAGS: -D_FILE_OFFSET_BITS=64
-
-#if defined(__clang__) && __clang_major__ >= 17
-#cgo CFLAGS: -Wno-typedef-redefinition -Wno-deprecated-non-prototype
-#endif
-
 */
 import "C"
